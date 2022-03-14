9bf220efc3cc0cacb8248bfcfe419100e4db92c3
gabriel.garbin <gabriel.garbin@FLE207D31.mshome.net>
2022-03-14 10:31:08 -0300

    atualizaçao teste

--- src/main/java/model/vo/Endereco.java
+++ src/main/java/model/vo/Endereco.java
@@ -1,40 +1,55 @@
 package model.vo;
 
 public class Endereco {
-
+	
 	private int id;
 	private String rua;
 	private String numero;
 	private String cidade;
 	private String uf;
 	private String cep;
-
+	
 	public Endereco() {
 	}
-
+	
 	public Endereco(String rua, String numero, String cidade, String uf, String cep) {
 		super();
 		this.rua = rua;
-		this.numero = numero;
 		this.cidade = cidade;
 		this.uf = uf;
 		this.cep = cep;
+		this.numero = numero;
 	}
-
+	
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getRua() {
 		return rua;
 	}
-
 	public void setRua(String rua) {
 		this.rua = rua;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getUf() {
+		return uf;
+	}
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
 	}
 
 	public String getNumero() {
@@ -44,29 +59,4 @@ public class Endereco {
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
-
-	public String getCidade() {
-		return cidade;
-	}
-
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
-	}
-
-	public String getUf() {
-		return uf;
-	}
-
-	public void setUf(String uf) {
-		this.uf = uf;
-	}
-
-	public String getCep() {
-		return cep;
-	}
-
-	public void setCep(String cep) {
-		this.cep = cep;
-	}
-
 }
