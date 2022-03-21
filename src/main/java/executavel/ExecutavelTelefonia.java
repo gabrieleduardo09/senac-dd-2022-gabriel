2cea6b1480174df75975440944d1d6cd7a2af612
gabriel.garbin <gabriel.garbin@FLE207D31.alunos.sc.senac.br>
2022-03-21 08:59:31 -0300

    atualização

--- src/main/java/executavel/ExecutavelTelefonia.java
+++ src/main/java/executavel/ExecutavelTelefonia.java
@@ -37,6 +37,7 @@ public class ExecutavelTelefonia {
 //		for(LinhaTelefonica linha: pele.getLinhas()) {
 //			System.out.println(linha.getTelefone().toString());
 //		}
+		//
 	}
 
 	private static void testarCrudEndereco() {
